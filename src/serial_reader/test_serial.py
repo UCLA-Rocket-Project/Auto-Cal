dd0ee8d1dbce45bc5e51b71c52ccae7108dab2bf
SleepyWoodpecker <shawnweichew@gmail.com>
2025-10-10 08:06:30 -0700

    fix: corrected testing example based on raw data format

--- src/serial_reader/test_serial.py
+++ src/serial_reader/test_serial.py
@@ -1,15 +1,30 @@
 import serial
+import struct
 
-ser = serial.Serial(port="/dev/tty.usbserial-0001", baudrate=115200, timeout=2)
+ser = serial.Serial(
+    port="/dev/cu.usbserial-0001",
+    baudrate=115200,
+    timeout=2,
+)
 cnt = 0
 print(ser.in_waiting, " bytes waiting to be read")
 
 while True:
     try:
-        ser.reset_input_buffer()
-        line = ser.read_until(b"\n").decode().strip()
-        print("received: ", line, cnt)
-        cnt += 1
+        line_hv = ser.read_until(b"\r\n", None)  # .rstrip(b'\r\n')
+
+        print(line_hv)
+        print(len(line_hv))
+        # Combine data from both ESP32s (HV first, then LV),
+        line = line_hv.rstrip(b"\r\n")  # HV first, then LV
+        print(line)
+        print(len(line))
+
+        # Unpack the raw bytes as floats
+        data = struct.unpack("8f", line)
+        print(",".join([f"{val:.2f}" for val in data]))
     except KeyboardInterrupt:
         ser.close()
         break
+    except:
+        pass
